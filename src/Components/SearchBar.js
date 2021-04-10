cb8724f5cdce10e3e1691a10b91fd79151c29dcf
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-10 22:10:53 +0100

    icons imported and added to the searchBar Component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,11 +1,14 @@
+import { FaMale, FaFemale} from "react-icons/fa";
+import { IoMaleFemale } from "react-icons/io5"
+
 const SearchBar = (props) => {
     return (
     <div className = "searchBar">
     <input onChange ={props.handleInput} className = "input" type = "text" placeholder = "search name..."></input>
     <div className = "gender">
-    <button onClick = {props.handleAll} className = "neutral">All</button>
-    <button onClick = {props.handleBoys} className = "babyBoys">Boys</button>
-    <button onClick = {props.handleGirls} className = "babyGirls">Girls</button>
+    <button onClick = {props.handleAll} className = "neutral"><IoMaleFemale className = "classIcon"/></button>
+    <button onClick = {props.handleBoys} className = "babyBoys"><FaMale className = "classIcon"/></button>
+    <button onClick = {props.handleGirls} className = "babyGirls"><FaFemale className = "classIcon"/></button>
     </div>
   </div>
     )
